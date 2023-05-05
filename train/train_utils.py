77309e9ac5abf282805de4c84a411658e3aa26b8
Yujin CHO <amycho92@gmail.com>
2023-05-05 14:05:18 +0200

    Update train_utils.py

--- train/train_utils.py
+++ train/train_utils.py
@@ -23,9 +23,17 @@ transformation = transforms.Compose(
      transforms.RandomHorizontalFlip(p=0.9),
      transforms.RandomVerticalFlip(p=0.9),
      transforms.RandomApply([transforms.RandomRotation((90, 90))], p=0.9),
-     #A.ImageCompression(quality_lower=10, quality_upper=100),
-     #A.Downscale(scale_min = 0.5, scale_max = 4),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+transformation2 = A.Compose([
+    A.RandomCrop(width=224, height=224),
+    A.augmentations.geometric.rotate.RandomRotate90(always_apply=False, p=1.0),
+    A.augmentations.geometric.transforms.HorizontalFlip(0.5),
+    A.augmentations.geometric.transforms.VerticalFlip(0.5),
+    A.augmentations.transforms.ImageCompression(quality_lower=10, quality_upper=90, p=0.9),
+    A.augmentations.transforms.Downscale(scale_limit=(0.25,4), p=0.9),
+    A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),ToTensorV2()
+])
 
 def imshow(img,title):
     img = img / 2 + 0.5  # unnormalize
