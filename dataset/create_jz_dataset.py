f892403767c2da10b9ade91ad3e8507b67a80757
Yujin CHO <amycho92@gmail.com>
2023-05-09 18:36:56 +0200

    Create create_jz_dataset.py

---
+++ dataset/create_jz_dataset.py
@@ -0,0 +1,4 @@
+from dataset.generate_dataset_for_forchheim import generate_dataset_for_forchheim
+dataset_path = r"/gpfsscratch/rech/jou/uru89tg/forchheim"
+output_path = r"/gpfsscratch/rech/jou/uru89tg/forchheim_preprocessed"
+generate_dataset_for_forchheim(folder_path=dataset_path, output_path=output_path, tiles_M = 224, tiles_N=224, nbr_patch_per_image=100, stride=224, shuffle = True)
