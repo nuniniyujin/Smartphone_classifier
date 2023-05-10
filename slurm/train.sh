c9b94845c8c6d04c906f5a9c11ab30a47c350eb5
Yujin CHO <amycho92@gmail.com>
2023-05-10 13:36:52 +0200

    Update train.sh
    modify runing time

--- slurm/train.sh
+++ slurm/train.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#SBATCH --nodes=1
 #SBATCH --job-name=forchheim_classifier
 #SBATCH --account=jou@v100
 #SBATCH --nodes=1
@@ -8,12 +7,11 @@
 #SBATCH --hint=nomultithread
 #SBATCH --cpus-per-task=3
 #SBATCH --qos=qos_gpu-t3
-#SBATCH --time=72:00:00
+#SBATCH --time=19:00:00
 #SBATCH --output=/gpfswork/rech/gbr/uru89tg/smartphone-classifier/logs/out/train_%j.txt
 #SBATCH --error=/gpfswork/rech/gbr/uru89tg/smartphone-classifier/logs/err/train_%j.txt
 #SBATCH --mail-user=thomas.eboli@ens-paris-saclay.fr
 # #SBATCH --partition=gpu_p2
-#SBATCH -C v100-32g
 
 # Clean modules
 module purge
@@ -22,7 +20,7 @@ module purge
 module load pytorch-gpu/py3/1.13.0
 
 cd /gpfswork/rech/gbr/uru89tg/smartphone-classifier/train
-pip install -r requirement.txt
+
 nvidia-smi
 free -m
 
